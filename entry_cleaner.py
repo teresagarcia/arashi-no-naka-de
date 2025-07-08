f704be6b3f3da4ddee19324c0f492a0a2458e9b3
Teresa García <tgarcia@kampal.com>
2025-07-08 14:11:55 +0200

    Improve entry cleaner

--- entry_cleaner.py
+++ entry_cleaner.py
@@ -3,13 +3,13 @@ import re
 with open("arashi_entries.json") as json_file:
     json_data = json.load(json_file)
 
-texto_sin_saltos = json_data[0]['content'].replace("\n", "")
-print(texto_sin_saltos)
+texto = re.sub(r'\n(?=<)', '', json_data[0]['content'])
+texto = re.sub(r'(?<=>)\n', '', texto)
+texto_sin_saltos = texto.replace("\n", " ")
 
-print(json_data[0]['title'])
-print(texto_sin_saltos)
 sin_comentarios = re.sub(r'<!--.*?-->', '', texto_sin_saltos, flags=re.DOTALL)
-resultado = re.sub(r'<(?!/div\b)[^>]+>', '', sin_comentarios)
+resultado = re.sub(r'<(?!/div\b)(?!a\b)(?!/a\b)[^>]+>', '', sin_comentarios)
 resultado = resultado.replace('</div>', '<br/>')
+resultado = re.sub(r'\s{2,}', ' ', resultado)
 print(resultado)
 
