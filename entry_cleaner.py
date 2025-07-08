710007d4f1882f24b0de915b391d060a8122522f
Teresa García <tgarcia@kampal.com>
2025-07-08 19:54:55 +0200

    First cleaner done

--- entry_cleaner.py
+++ entry_cleaner.py
@@ -28,3 +28,36 @@ result = re.sub(r'\s{2,}', ' ', result)
 result = re.sub(r'<br\s*/?>(?!\s*$)', '<br/>\n', result)
 print(result)
 
+# Separar letra e info
+lines = result.splitlines()
+
+for i, line in enumerate(lines):
+    if "Créditos:" in line or "Info de la canción" in line:
+        index = i
+        break
+
+lyrics = "\n".join(lines[:index])     
+song_info = "\n".join(lines[index:])     
+
+print("Sólo letra:\n", lyrics)
+print("Créditos:\n", song_info)
+
+# Separar partes del título
+title = json_data[0]['title'].replace('[Letra]', '')
+title_parts = title.split('-')
+artist = title_parts[0].strip()
+song = title_parts[1].strip()
+
+print("Artista:", artist, ", song:", song)
+
+
+#Save data in json file
+with open("clean_entries.json", "r", encoding="utf-8") as f:
+    clean_entries = json.load(f)
+
+new_entry = {"artist": artist, "song": song, "lyrics": lyrics, "info": song_info, "blogger_link": json_data[0]['link']}
+clean_entries.append(new_entry)
+
+# 3. Guardar de nuevo
+with open("clean_entries.json", "w", encoding="utf-8") as f:
+    json.dump(clean_entries, f, indent=2, ensure_ascii=False)
