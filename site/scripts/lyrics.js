0ff19c264187368682ae3a941d9d3e0edf8e9c2d
Teresa García <tgarcia@kampal.com>
2025-07-09 12:52:37 +0200

    Load lyrics from json

---
+++ site/scripts/lyrics.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", async () => {
+  const params = new URLSearchParams(window.location.search);
+  const lyricsFile = params.get('song') ?? '/5x10/truth.json';
+
+  setInterval(async () => {
+    if (document.querySelector('h2') !== null) {
+      const dataRes = await fetch(lyricsFile);
+      const data = await dataRes.json();
+      document.querySelector('h2').textContent = `[Letra] ${data.artist} - ${data.song}`;
+      document.querySelector('.intro').innerHTML = data.intro;
+      document.querySelector('.content').innerHTML = data.content;
+      document.querySelector('.info').innerHTML = data.info;
+    }
+  }, 5)
+
+})
