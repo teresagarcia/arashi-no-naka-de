5d883dc39c5993ad34fbe04fbb741417fc83b48a
Teresa García <tgarcia@kampal.com>
2025-07-09 17:44:58 +0200

    Update link

--- site/scripts/lyrics.js
+++ site/scripts/lyrics.js
@@ -2,11 +2,12 @@ window.addEventListener("load", async () => {
   const params = new URLSearchParams(window.location.search);
   const lyricsFile = params.get('song');
 
-  const dataRes = await fetch(lyricsFile);
+  const dataRes = await fetch(`${lyricsFile}.json`);
   const data = await dataRes.json();
-  document.querySelector('h2').textContent = `[Letra] ${data.artist} - ${data.song}`;
+  const entryTitle = `[Letra] ${data.artist} - ${data.song}`;
+  document.querySelector('h2').textContent = entryTitle;
   document.querySelector('.intro').innerHTML = data.intro;
   document.querySelector('.content').innerHTML = data.content;
   document.querySelector('.info').innerHTML = data.info;
-
+  document.title = `${entryTitle} | Arashi no naka de`;
 })
