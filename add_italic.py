d5927c91dd8b25551c933c22b37a875a185131dd
Teresa García <tgarcia@kampal.com>
2025-07-11 14:05:21 +0200

    add italic working :DD

--- add_italic.py
+++ add_italic.py
@@ -5,11 +5,17 @@ with open("clean_entries.json") as json_file:
 
 lyrics = json_data[0]['lyrics']
 
-print("lyrics sin arreglar\n", lyrics)
-
 cachitos = lyrics.split('<br/>\n<br/>')
 
+new_cachitos = []
 for i,cachito  in enumerate(cachitos):
-    print(i, cachito)
-    print("varias líneas", cachito.splitlines().__len__() > 1)
-#si es una línea <i> en esa línea, si dos o más, en la segunda desde el final
+    multiline = cachito.splitlines().__len__() > 1
+    if multiline :
+        cachitos_a_cachitos = cachito.splitlines()
+        parte1, sep, _ = cachitos_a_cachitos[-2].partition('<br/>')
+        cachitos_a_cachitos[-2] = f"<i>{parte1}</i>{sep}"
+        cachitos_joined = ''.join(cachitos_a_cachitos)
+        new_cachitos.append(cachitos_joined)
+
+final_result = '<br/>\n<br/>'.join(new_cachitos) + '<br/>\n<br/>'
+print(final_result)
