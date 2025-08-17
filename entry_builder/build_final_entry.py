7b607658fcb0b64cf0e410af9b1110fdcdbfa67f
Teresa García <teresagarciaegea@gmail.com>
2025-08-17 18:52:10 +0200

    Add "together, forever"

--- entry_builder/build_final_entry.py
+++ entry_builder/build_final_entry.py
@@ -17,19 +17,19 @@ def limpiar_para_url(texto):
 def final_transformation():   
     with open("data/preformat_entry.json") as json_file:
         json_data = json.load(json_file)
-    lyricist = 'Tatsurō Mashiko'
-    composer = 'Tatsurō Mashiko'
-    arranger = 'Tatsurō Mashiko'
-    album = 'Boku no Miteiru Fuukei (2010)'
-    album_folder = "boku_no_miteiru_fuukei"
-    translations_info = "Letra en japonés, romaji y traducción al inglés: <a href=\"http://yarukizero.livejournal.com/20098.html\" target=\"_blank\" class='text-blue-link'>Yarukizero</a>"
+    lyricist = '100+'
+    composer = '100+'
+    arranger = 'Hirofumi Sasaki'
+    album = 'Meikyuu Love Song (2011)'
+    album_folder = "meikyuu_love_song"
+    translations_info = "Letra en japonés: <a href=\"http://www.littleoslo.com/lyj/home/2011/10/%E5%B5%90-together-forever/\" target=\"_blank\">Oo Kashi</a>.<br/>\nRomaji y traducción al inglés: <a href=\"http://www.jpopasia.com/group/arashi/lyrics/meikyu-love-song/together-forever::71798.html\" target=\"_blank\">JpopAsia</a><br/>"
     
     credits_section = f"<span class='font-bold text-blue-secondary'>Créditos:</span><br />{translations_info}.<br/>Traducción al español publicada originalmente en <a href='https://yasashiiuta05.blogspot.com{json_data['blogger_link']}' target='_blank' class='text-blue-link'>Yasashii Uta</a>, mantenida y actualizada en <a href='https://arashinonakade.neocities.org/about.html' class='text-blue-link'>Arashi no naka de</a>."
 
     new_entry = {
         'artist': json_data['artist'],
         'song': json_data['song'],
-        'intro': '<i>Boku no Miteiru Fuukei</i> nos dio muchísimas joyas, entre ellas esta. Siempre me llena de melancolía y nostalgia pero también de la fuerza serena que estos chicos siempre saben transmitir.',
+        'intro': 'Tierna canción dentro del single <i>Meikyuu Love Song</i> &#10084;',
         'content': json_data['lyrics'],
         'lyricist': lyricist,
         'composer': composer,
